4ddb2515434d77c46787b92c41d00e28ad11bfb5
Tyler <shed412@gmail.com>
2016-09-04 02:28:29 -0400

    ...I think I did it.

--- RecRabbit.java
+++ RecRabbit.java
@@ -4,24 +4,76 @@ public class RecRabbit
 {
     public static void main(String[] args)
     {
-         int[] x = {8,2,3,5,1,2,12,4,7,41,5,7,4,124};
-         answers(x);
-        //int[] x = {12,0,0,0,0,4};
-        //moveRabbits(0, 5, x);
+        Random rng = new Random();
+        int[] x = new int[(rng.nextInt(100)+2)];
+
+        //System.out.println(x.length+"\n\n");
+
         for(int i=0; i<x.length; i++){
-            System.out.print(x[i]+" \n");
+            x[i] = rng.nextInt(1000000);
         }//for
+        System.out.println(answer(x));
 
-
-
-
+        for(int i=0; i<x.length; i++){
+            System.out.println(x[i]+" ");
+        }
     }
 
-    public static int answers(int[] x)
+    public static int answer(int[] x)
     {
+        int sum = 0;
+        int avg = 0;
+        for(int i=0; i<x.length; i++){
+            sum+=x[i];
+        }//for
+        avg = (sum/x.length);
         total(0, x.length-1, x);
-        return 0;
+        distribute(0, x.length-1, x, avg);
+        if(x[0]>avg){
+            distribute(0, x.length-1, x, 1);
+        }
+        return count(x, avg);
     }//answers
+
+    public static int count(int[] arr, int avg)
+    {
+        int a1 = 0;
+        int a2 = 0;
+        int avg2 = 0;
+
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] == avg){
+                a1++;
+            }else{
+                a2++;
+                avg2 = arr[i];
+            }//if else
+        }//for
+
+        if(a1>=a2){
+            return a1;
+        }else{
+            return a2;
+        }
+    }//count
+
+
+    public static void distribute(int lp, int hp, int[] x, int avg)
+    {
+        if (lp<hp){
+            int middle = lp + (hp-lp)/2;
+            disRabbits(lp, middle+1, x, avg);
+            distribute(lp, middle, x, avg);
+            distribute(middle+1, hp, x, avg);
+        }//if
+    }//distribute
+
+    public static void disRabbits(int lp, int mp, int[] x, int avg)
+    {
+        for(int i=0; i<avg && x[0]>x[x.length-1]; i++){
+            move(mp, 0, x);
+        }//for
+    }//disRabbits
 
     public static void total(int lp, int hp, int[] x)
     {
@@ -29,14 +81,13 @@ public class RecRabbit
           int middle = lp + (hp-lp)/2;
           total(lp, middle, x);
           total(middle+1, hp, x);
-          moveRabbits(lp, middle, hp, x);
-      }
+          conRabbits(lp, hp, x);
+      }//if
     }//total
 
-    public static void moveRabbits(int lp, int middle, int hp, int[] x)
+    public static void conRabbits(int lp, int hp, int[] x)
     {
       int nump = 0;
-
 
           for(int i=lp+1; i<=hp; i++){
             if(x[i]>0){
@@ -47,7 +98,6 @@ public class RecRabbit
           while(x[nump]>0){
               move(lp, nump, x);
           }//while
-
     }//moveRabbits
 
     public static void move(int lp, int hp, int[] x)
