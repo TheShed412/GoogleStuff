8ba5e823fd56ceb84f0f10e28404b66b9c273bda
Tyler <shed412@gmail.com>
2016-09-03 22:14:38 -0400

    Recursive sort of working

--- RecRabbit.java
+++ RecRabbit.java
@@ -2,5 +2,50 @@ import java.util.*;
 
 public class RecRabbit
 {
-  
+    public static void main(String[] args)
+    {
+        int[] x = {8,2,3,5,1,2,12,4,7,41};
+        answers(x);
+        for(int i=0; i<x.length; i++){
+            System.out.print(x[i]+" \n");
+        }//for
+    }
+
+    public static int answers(int[] x)
+    {
+        total(0, x.length-1, x);
+        return 0;
+    }//answers
+
+    public static void total(int lp, int hp, int[] x)
+    {
+        if(lp < hp){
+          int middle = lp + (hp-lp)/2;
+          total(lp, middle, x);
+          total(middle+1, hp, x);
+          moveRabbits(lp, hp, x);
+      }
+    }//total
+
+    public static void moveRabbits(int lp, int hp, int[] x)
+    {
+      int nump = 0;
+      for(int i=lp; i<=hp; i++){
+        if(x[i]>0){
+          nump = i;
+        }//if
+      }//for
+
+      if(x[nump]>x[lp]){
+          while(x[nump]>0){
+                  move(lp, nump, x);
+          }//while
+      }//if
+    }//moveRabbits
+
+    public static void move(int lp, int hp, int[] x)
+    {
+      x[lp] ++;
+      x[hp] --;
+    }//move
 }
